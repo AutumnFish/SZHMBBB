985905ebdae142fb1bfcad421a8b8081274b3385
autumnFish <517729329@qq.com>
2018-07-25 17:06:27 +0800

    搭建好项目的基本结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+// 引入css
+import './assets/statics/site/css/style.css';
+
 Vue.config.productionTip = false
 
 new Vue({
+  // 选择器
+  el:"#app",
+  // 渲染 App组件
   render: h => h(App)
-}).$mount('#app')
+})
